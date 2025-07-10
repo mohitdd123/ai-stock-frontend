8c916d4ffe70f9d180df429a77458232822e59d2
mohitdd123 <mohitdd123@gmail.com>
2025-07-10 18:41:01 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://your-backend-url.onrender.com';
-
-export const fetchScreenedStocks = async () => {
-  const res = await axios.get(`${BASE_URL}/screened/stocks`);
-  return res.data;
-};
