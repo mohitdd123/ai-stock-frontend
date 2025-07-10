2c797051fb1c21543ad284a69305112239acb8d2
mohitdd123 <mohitdd123@gmail.com>
2025-07-10 07:46:46 +0530

    Delete client directory

--- client/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://ai-stock-screener.onrender.com';
-
-export const fetchScreenedStocks = async () => {
-  const res = await axios.get(`${BASE_URL}/screened/stocks`);
-  return res.data;
-};
